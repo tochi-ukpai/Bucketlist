703339edf717ce3510ded290187b69750dbf2f44
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-06-15 00:13:59 +0100

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Theós on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
